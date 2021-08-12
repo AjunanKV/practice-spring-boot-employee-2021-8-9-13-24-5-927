309f744d8955d50f5af2ddd9aac30225736849f4
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-12 22:40:29 +0800

    [employees][Kevin]test: add integration test: should_return_all_employees_when_get_all_employees_API

--- src/test/java/com/thoughtworks/springbootemployee/IntegrationTest/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/IntegrationTest/EmployeeIntegrationTest.java
@@ -30,44 +30,11 @@ public class EmployeeIntegrationTest {
         //then
         mockMvc.perform(MockMvcRequestBuilders.get("/employees"))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].name").value("Cedric"))
-                .andExpect(jsonPath("$[0].age").value(21))
+                .andExpect(jsonPath("$[0].name").value("Linne"))
+                .andExpect(jsonPath("$[0].age").value(20))
                 .andExpect(jsonPath("$[0].gender").value("female"))
                 .andExpect(jsonPath("$[2].name").value("Janelle"))
                 .andExpect(jsonPath("$[2].age").value(20))
                 .andExpect(jsonPath("$[2].gender").value("female"));
     }
-//    @Test
-//    public void should_return_employee_when_get_employee_given_employee_id() throws Exception {
-//
-//        //given
-//        int tempId = 1;
-//        mockMvc.perform(MockMvcRequestBuilders.get("/employees/{employeeid}/",tempId))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$[0].name").value("Cedric"))
-//                .andExpect(jsonPath("$[0].age").value(19))
-//                .andExpect(jsonPath("$[0].gender").value("male"));
-//    }
-    @Test
-    public void should_create_employee_when_call_create_employee_api() throws Exception {
-        String employee = "{\n" +
-                " \"id\": 1, \n" +
-                " \"name\": \"Linne\", \n" +
-                " \"age\": 20, \n" +
-                " \"gender\": \"female\", \n" +
-                " \"salary\": 1000\n" +
-                "}";
-
-        //when
-        //then
-        mockMvc.perform(MockMvcRequestBuilders.post("/employees")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(employee))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$[4].name").value("Linne"))
-                .andExpect(jsonPath("$[4].age").value(20))
-                .andExpect(jsonPath("$[4].gender").value("female"));
-    }
-
-
 }
