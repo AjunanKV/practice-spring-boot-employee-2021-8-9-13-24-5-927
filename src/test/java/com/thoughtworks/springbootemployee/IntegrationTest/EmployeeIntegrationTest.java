23ff49c3ad4bd6cd9d5f00c0335332c30da46468
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-12 22:47:33 +0800

    [employees][Kevin]feat: add implementation to: should_create_employee_when_call_create_employee_api

--- src/test/java/com/thoughtworks/springbootemployee/IntegrationTest/EmployeeIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/IntegrationTest/EmployeeIntegrationTest.java
@@ -58,7 +58,7 @@ public class EmployeeIntegrationTest {
                 .andExpect(jsonPath("$.age").value(20))
                 .andExpect(jsonPath("$.gender").value("male"));
     }
-    
+
 
 
 }
