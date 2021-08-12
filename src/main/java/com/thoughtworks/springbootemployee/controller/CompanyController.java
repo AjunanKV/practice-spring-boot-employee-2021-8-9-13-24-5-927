004fe03e24cc02fe80246ef0bbb3c7784d7b5055
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-12 22:31:06 +0800

    [employees][Kevin]feat: add CompanyController together with getmapping: getAllEmployeesByCompanyId

---
+++ src/main/java/com/thoughtworks/springbootemployee/controller/CompanyController.java
@@ -0,0 +1,27 @@
+package com.thoughtworks.springbootemployee.controller;
+
+import com.thoughtworks.springbootemployee.model.Company;
+import com.thoughtworks.springbootemployee.model.Employee;
+import com.thoughtworks.springbootemployee.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/companies")
+public class CompanyController {
+    @Autowired
+    private CompanyService companyService;
+
+    private List<Company> companies = new ArrayList<>();
+
+    @GetMapping(path = "/{companyId}/employees")
+    public List<Employee> getAllEmployeesByCompanyId(@PathVariable Integer companyId){
+        return companyService.getAllEmployeesByCompanyId(companyId);
+    }
+}
