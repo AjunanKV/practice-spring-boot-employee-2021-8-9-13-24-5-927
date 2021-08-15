e3aab1b29c8c854d791e14b336c2bf7005b55e77
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-15 19:56:25 +0800

    [employees][Kevin]feat: add getters and setters'

--- src/main/java/com/thoughtworks/springbootemployee/model/EmployeeRequest.java
+++ src/main/java/com/thoughtworks/springbootemployee/model/EmployeeRequest.java
@@ -12,6 +12,46 @@ public class EmployeeRequest {
 
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public Integer getSalary() {
+        return salary;
+    }
+
+    public void setSalary(Integer salary) {
+        this.salary = salary;
+    }
+
+    public Integer getCompanyid() {
+        return companyid;
+    }
+
+    public void setCompanyid(Integer companyid) {
+        this.companyid = companyid;
+    }
+
     public EmployeeRequest(String name, Integer age, String gender, Integer salary, Integer companyid) {
         this.name = name;
         this.age = age;
